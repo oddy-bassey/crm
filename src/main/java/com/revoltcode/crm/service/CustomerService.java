f68fa2e4ffb5c95f2a345b401611a440a650bce6
oddy-bassey <oddybassey@gmail.com>
2022-06-11 08:15:05 +0100

    implemented the service layer

---
+++ src/main/java/com/revoltcode/crm/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.revoltcode.crm.service;
+
+import com.revoltcode.crm.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    Optional<Customer> findByCustomerId(UUID customerId);
+    Optional<Customer> findByEmail(String email);
+    List<Customer> findAll();
+    Customer save(Customer customer);
+    void deleteCustomer(Customer customer);
+}
---
+++ src/main/java/com/revoltcode/crm/service/CustomerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.revoltcode.crm.service;
+
+import com.revoltcode.crm.model.Customer;
+import com.revoltcode.crm.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Optional<Customer> findByCustomerId(UUID customerId) {
+        return customerRepository.findById(customerId);
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email){
+        return customerRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public void deleteCustomer(Customer customer) {
+        customerRepository.delete(customer);
+    }
+}
