37635db8141a8afb1c1b22ee4237786e33523c25
oddy-bassey <oddybassey@gmail.com>
2022-06-11 20:47:57 +0100

    created integration test for DB

---
+++ src/test/java/com/revoltcode/crm/CrmDbIntegrationTests.java
@@ -0,0 +1,35 @@
+package com.revoltcode.crm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestPropertySource("/application.properties")
+@SpringBootTest
+public class CrmDbIntegrationTests {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    public void populateDatabase(){
+        jdbcTemplate.execute("insert into customer (CREATED_DATE, DATE_OF_BIRTH, EMAIL, FIRST_NAME, GENDER, LAST_NAME, ID) " +
+                "values ('2022-06-11 20:09:14', '2021-09-12', 'dami@gmail.com', 'dami', 'female', 'adams', 'ef3b95f1-01b1-4dcf-9a65-0bb798e159c1')");
+    }
+
+    @AfterEach
+    public void cleanDatabase(){
+        jdbcTemplate.execute("delete from customer");
+    }
+
+    @Test
+    public void dbContainsData(){
+        int numberOfCustomersSaved = jdbcTemplate.queryForObject("select count(*) from customer", Integer.class);
+        assertTrue(numberOfCustomersSaved>0);
+    }
+}
