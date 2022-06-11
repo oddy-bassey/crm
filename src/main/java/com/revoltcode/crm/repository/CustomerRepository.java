f8958bcf049ca0878df8e1c97633cd4277259f9a
oddy-bassey <oddybassey@gmail.com>
2022-06-11 08:13:10 +0100

    implemented customer repository

---
+++ src/main/java/com/revoltcode/crm/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.revoltcode.crm.repository;
+
+import com.revoltcode.crm.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByEmail(String email);
+}
