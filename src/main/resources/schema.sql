0c57f9d1029e3d51eea658c491b2eb728beffe98
oddy-bassey <oddybassey@gmail.com>
2022-06-14 09:28:27 +0100

    setup DB creation and initialization using sql scripts

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+drop table if exists customer CASCADE;
+create table customer (ID VARCHAR(36) not null, CREATED_DATE timestamp not null, DATE_OF_BIRTH date not null, EMAIL varchar(255), FIRST_NAME varchar(255), GENDER varchar(255) not null, LAST_NAME varchar(255), primary key (ID));
+alter table customer add constraint UKdwk6cx0afu8bs9o4t536v1j5v unique (email);
